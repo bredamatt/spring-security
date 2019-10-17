39b13dc4f015e9f84e7fae114fd967aa0b6d3c94
neologist91 <mlvb91@icloud.com>
2019-10-17 13:06:22 +0200

    Added skeleton

---
+++ src/test/java/com/springsec/restfws/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsec.restfws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
